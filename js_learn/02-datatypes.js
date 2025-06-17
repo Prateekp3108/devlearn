1f48778e824c26cb1e8753a36f10fc03633996be
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-17 20:08:34 +0530

    datatypes learned

--- js_learn/02-datatypes.js
+++ js_learn/02-datatypes.js
@@ -1,3 +1,20 @@
 "use strict"; //treat all js code as newer version
 //alert(3+3) //this doesnt work in nodjs,but works in browser
 console.log();
+
+
+let name="prateek" //string
+let age=18 // int
+let isloggedin= false //boolean
+
+
+//number=> 2^53 
+//bigint
+//striing=>"hgk"
+//boolean=> true/false
+//null=>stanalone value
+//undefined=> no value assinged
+//symbol => unique
+//object 
+console.log(typeof null); //output => object
+
