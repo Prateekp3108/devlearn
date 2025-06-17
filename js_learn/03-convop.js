baa31cc12a877234bb28a94d173f620cac0ccd8e
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-17 20:53:12 +0530

    datatype conversion operation

---
+++ js_learn/03-convop.js
@@ -0,0 +1,28 @@
+let score = 33;
+let score1 = "33";
+console.log(typeof (score));
+console.log(typeof (score1));
+
+let valinnum=Number(score1)
+console.log(typeof valinnum);
+console.log(valinnum); //output value=> NaN
+
+//mdn https://developer.mozilla.org/en-US/docs/Web/JavaScript
+//tc39.es https://tc39.es/ecma262/
+
+//"33"=>33
+//"33asd"=>NaN
+
+
+let islogin=1 
+let boolislogin=Boolean(islogin)
+console.log(boolislogin);
+
+//true=1 false=0
+//""=>false
+//"prateek"=>true
+
+let somenum=343
+let stringsomenum= String(somenum)
+console.log(stringsomenum)
+console.log(typeof (stringsomenum))
