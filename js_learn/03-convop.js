b75b60f51bdb1d9e4185eeae3746fa1e0507da99
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-18 15:47:33 +0530

    summary read

--- js_learn/03-convop.js
+++ js_learn/03-convop.js
@@ -1,11 +1,11 @@
 let score = 33;
 let score1 = "33";
-// console.log(typeof (score));
-// console.log(typeof (score1));
+console.log(typeof (score));
+console.log(typeof (score1));
 
 let valinnum=Number(score1) 
-// console.log(typeof valinnum);
-// console.log(valinnum); //output value=> NaN
+console.log(typeof valinnum);
+console.log(valinnum); //output value=> NaN
 
 //mdn https://developer.mozilla.org/en-US/docs/Web/JavaScript
 //tc39.es https://tc39.es/ecma262/
@@ -16,43 +16,43 @@ let valinnum=Number(score1)
 
 let islogin=1 
 let boolislogin=Boolean(islogin)
-// console.log(boolislogin);
+console.log(boolislogin);
 
 
-//true=1 false=0
-//""=>false
-//"prateek"=>true
+// true=1 false=0
+// ""=>false
+// "prateek"=>true
 
 let somenum=343
 let stringsomenum= String(somenum)
-// console.log(stringsomenum)
-// console.log(typeof (stringsomenum))
+console.log(stringsomenum)
+console.log(typeof (stringsomenum))
 
 // operations
 
 let val=4
 let negval=-val
-// console.log(negval);
+console.log(negval);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**11); power
-// console.log(2/2);
-// console.log(2%2); remainder
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**11);//power
+console.log(2/2);
+console.log(2%2); //remainder
 
 
 
-// let str1="prateek"
-// let str2=" pandey"
-// let str3=str1+str2;
-// console.log(str3);
+let str1="prateek"
+let str2=" pandey"
+let str3=str1+str2;
+console.log(str3);
 
-// console.log("1"+2+2);//=122
-// console.log(1+2+"2"); //=32
+console.log("1"+2+2);//=122
+console.log(1+2+"2"); //=32
 
-// console.log(+true);
-//console.log(+" ");
+console.log(+true);
+console.log(+" ");
 let num,num1,num2
 num1=num2=num3=2+2
  
