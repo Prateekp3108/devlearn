479fbd68702db6175690f023460f5355ffc82c63
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-20 17:19:42 +0530

    string functions learned

---
+++ js_learn/05-string.js
@@ -0,0 +1,28 @@
+const name="prateek pandey"
+const repoc=50
+
+// console.log(name +" "+ repoc+ " value");
+
+// console.log(`hello my name is ${name} and my rep count is ${repoc}`);
+
+const gamename= new String('prateek')
+
+// console.log(gamename);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('r'));
+// console.log(gamename.toUpperCase());
+// const newstr=gamename.substring(0,5) //no negative values work
+// console.log(newstr);
+// const anostr=gamename.slice(3,7)  // neg values work
+// console.log(anostr);
+
+const newstr2="    prateek     "
+console.log(newstr2);
+console.log(newstr2.trim());
+
+const url="https://prateek.com/prateek%20pandey"
+console.log(url.replace('%20','-'));
+
+console.log(url.split(''))
+ 
