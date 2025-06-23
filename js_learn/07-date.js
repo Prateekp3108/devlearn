15bcacd764c68ee803bb96cb4792bd6da4d755b9
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-23 19:28:56 +0530

    date and time learned

---
+++ js_learn/07-date.js
@@ -0,0 +1,27 @@
+//dates
+// let mydate= new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(typeof mydate);
+
+let mycd=new Date(2034,7,31,8,45)
+// console.log(mycd.toLocaleString());
+// console.log(mycd.toDateString());
+
+// let myTimeStamp= Date.now()
+// console.log(mycd.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getMinutes());
+console.log(newDate.getSeconds());
