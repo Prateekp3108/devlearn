4c470f9eb88e3d2a62e1be7ed56af7846259ef19
Prateekp3108 <prateekpandey3108@gmail.com>
2025-06-23 17:31:46 +0530

    nums and nath library learned

---
+++ js_learn/06-nums-maths.js
@@ -0,0 +1,31 @@
+const score = 60;
+
+const balance = new Number(100.23998878765443347785);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+// console.log(balance.toPrecision(10));
+
+const hunds=10000;
+// console.log(hunds.toLocaleString('en-IN'))
+
+// mathsss
+const num=new Number(4.499+0.01)
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.min(4,7,5));
+// console.log(Math.max(4,7,5));
+
+const prat=Math.random()
+console.log(prat.toFixed(3)*10);
+console.log(((prat.toFixed(0))*10)+1);
+
+const min=34
+const max=43
+
+console.log(Math.random()*(max-min)+min);
+
