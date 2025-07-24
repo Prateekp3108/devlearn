cba538a3d3da7c18b6307984fdca6d263b1f0e85
Prateekp3108 <prateekpandey3108@gmail.com>
2025-07-24 13:40:38 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,37 @@
+// var c=300
+// let a=300
+// if(true)
+// {
+//   let a=10;
+//   const b=20;
+//   // var c=30;
+//   console.log(a);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+  const username="prateek"
+  function two (){
+    const website="youtube"
+    console.log(username);
+  }
+  // console.log(website);
+  // two();
+}
+one();
+
+
+//************************** 
+
+// console.log(addone(5))
+function addone(num){//how is the function declared 
+  return num+1;
+}
+
+// console.log(addtwo(5));
+const addtwo=function(num){
+  return num+2
+}
+  
